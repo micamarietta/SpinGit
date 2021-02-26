0a5955893ebd1dfc5fbf125d6d2864f2d6d73c8b
micamarietta <micamarietta@gmail.com>
2021-02-26 13:31:16 -0800

    Initial UI Commit
    Basic UI text, screens, buttons
    Basic SceneManager Script
    Temp Wheel Button for Wheel code

---
+++ Unity Spin/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void LoadSplashScreen()
+    {
+        SceneManager.LoadScene(0);
+        Debug.Log("Heading to the splash scene");
+    }
+
+    public void LoadMainScreen()
+    {
+        SceneManager.LoadScene(1);
+        Debug.Log("Heading to the main scene");
+    }
+
+    public void LoadDefaultSpinnerScript()
+    {
+        SceneManager.LoadScene(3);
+        Debug.Log("Heading to the default spinner");
+    }
+
+    public void LoadCreateListScreen()
+    {
+        SceneManager.LoadScene(2);
+        Debug.Log("Heading to the create list scene");
+    }
+
+}
