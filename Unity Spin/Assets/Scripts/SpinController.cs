cad7cd7a02601f683fdcddbc9062b0426cf2f42f
micamarietta <micamarietta@gmail.com>
2021-03-02 12:13:44 -0800

    Spin Controller
    Added changes to spin controller script to make the spinner spin when clicked

--- Unity Spin/Assets/Scripts/SpinController.cs
+++ Unity Spin/Assets/Scripts/SpinController.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class SpinController : MonoBehaviour
 {
 
     float rotSpeed = 0;
+    public GameObject spinnerButton;
 
     private void Update()
     {
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && EventSystem.current.currentSelectedGameObject == spinnerButton)
         {
-            this.rotSpeed = 10;
+            this.rotSpeed = 20;
         }
-     
+
         transform.Rotate(0, 0, this.rotSpeed);
 
         this.rotSpeed *= 0.96f;
