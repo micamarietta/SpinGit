47c37c185bbd93840fb5bd807d8f5bcdbd78bae5
Cole Ternes <ternes@chapman.edu>
2021-03-14 20:41:10 -0700

    Spinner Objects & PlayerPrefs
    Added ManageSpinners.cs
    Added PlayerPrefsX.cs
    Adjusted CustomSpinner.cs to be a scriptable object

---
+++ Unity Spin/Assets/Scripts/CustomSpinner.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// This class is a scriptable object: https://docs.unity3d.com/Manual/class-ScriptableObject.html
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/CustomSpinner", order = 1)]
+
+public class CustomSpinner : ScriptableObject
+{
+    // BIG TODO:
+    // This class needs a button overhaul to connect the methods to the UI; I'm neither expertized nor brave enough to try - Cole
+
+    // The locations that PlayerPrefs are stored on iOS and Android: https://answers.unity.com/questions/131511/playerprefs-storage-location-on-android-or-ios.html
+    private List<string> tmpActivities; // a temmporary List of the activities (strings) to allow the user to freely edit the spinner
+    public string[] savedActivities; // all of the final activities found in the List are then transferred to this array which will be stored using PlayerPrefsX
+    public string title; // titles are used as the "key" for the PlayerPrefsX so we know which one to access
+    //NOTE: the following 2 TODO's are not planned to be finished in Sprint #2
+    //Future TODO: add a variable to store an image that represents the spinner on the Main Menu 
+    //Future TODO: add a vector to correctly display the image on the Main Menu
+
+    // NOTE: I kept these three variables because I don't know exactly what they do nor how they work; I know Pablo used them with the AddToSpinner() method - Cole
+    // Source of original code which Pablo modified: https://answers.unity.com/questions/1101002/adding-a-new-string-to-my-array-after-each-button.html
+    public Button addActivity;
+    public Canvas canvas;
+    public InputField activity;
+
+    /*
+    NOTE: I'm not sure if this still needs to be included or if it will be modified with the button overhaul
+    public void Start()
+    {
+        addActivity = addActivity.GetComponent<Button>();
+        canvas = canvas.GetComponent<Canvas>();
+        activity = activity.GetComponent<InputField>();
+    }
+    */
+
+    public CustomSpinner() // Normal constructor to build new spinner objects
+    {
+        // TODO: declare the title like how its specified in the parantheses
+        // title = (needs to be the user input passed in from the textbox && can't be the same as any existing titles found in the "storedTitles" array)
+        savedActivities = new string[100]; // max size is 100
+    }
+
+    public CustomSpinner(string storedTitle) // Overloaded constructor used to load the pre-existing spinners stored in PlayerPrefsX
+    {
+        title = storedTitle;
+        for (int i = 0; i < PlayerPrefsX.GetStringArray(storedTitle).Length; ++i)
+        {
+            tmpActivities[i] = PlayerPrefsX.GetStringArray(storedTitle)[i];
+        }
+        savedActivities = new string[100]; // Max size is 100
+    }
+
+    public string GetTitle()
+    {
+        return title;
+    }
+
+    public void AddToSpinner() // Adds a new activity to the tmp list
+    {
+        //TODO: double check that these buttons still work
+        if (tmpActivities.Count <= 100) // checks to make sure the List is less than 100
+        {
+            tmpActivities.Add(activity.text);
+        }
+        //TODO: have an else-statement that will display a pop-up to the user that they have too many items
+
+        Debug.Log("index: " + (tmpActivities.Count-1) + ", input: " + activity.text);
+    }
+
+    public void RemoveFromSpinner() // Removes an activity from the tmp List
+    {
+        //TODO: incorporate a button that indicates the index at which an activity needs to be removed
+        //tmpActivities.RemoveAt("index")
+    }
+
+    public void SaveSpinner() // Saves the spinner into a PlayerPrefsX array
+    {
+        //TODO: incorporate a button to make this method functional
+        for (int i = 0; i < tmpActivities.Count; ++i) // Transfers the activities in the temporary List to the array that will be stored
+        {
+            savedActivities[i] = tmpActivities[i];
+        }
+
+        PlayerPrefsX.SetStringArray(title, savedActivities);
+        tmpActivities.Clear();
+    }
+
+    public void DeleteSpinner()
+    {
+        for (int i = 0; i < PlayerPrefsX.GetStringArray("storedSpinnerTitles").Length; ++i) // Cycles through the PlayerPrefsX that stores all of the titles
+        {
+            if (PlayerPrefsX.GetStringArray("storedSpinnerTitles")[i] == title) // Nullifies the title found in the PlayerPrefsX
+            {
+                PlayerPrefsX.GetStringArray("storedSpinnerTitles")[i] = null;
+                break;
+            }
+        }
+
+        PlayerPrefsX.SetStringArray(title, null); // Nullifies the array stored with the given title keyword
+    }
+
+    /*
+    NOTE: This was a part of Pablo's original code and it was tied to AddToSpinner()
+    NOTE: I'm not sure if it still needs to be included or if it will be modified with the button overhaul
+    public void OnButtonClick()
+    {
+        AddToSpinner();
+    }
+    */
+}
---
+++ Unity Spin/Assets/Scripts/ManageSpinners.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageSpinners : MonoBehaviour
+{
+    // BIG TODO:
+    // Incorporate the functionality from SpinnerMechanics.cs into either CustomSpinner.cs or ManageSpinners.cs (whichever
+    // makes the most sense) so that Unity displays curSpinner instead of Pablo's manually set List
+
+    public List<CustomSpinner> allSpinners; // a dynamic list that stores all CustomSpinner objects
+    public CustomSpinner curSpinner; // the current spinner that is either being created or editted
+    public string[] storedSpinnerTitles;
+
+    void Start() // Loads the stored spinners on the start of the app
+    {
+        if (PlayerPrefsX.GetStringArray("storedSpinnerTitles").Length != 0) // Checks if the user's storedSpinnerTitles array is not empty
+        {
+            for (int i = 0; i < PlayerPrefsX.GetStringArray("storedSpinnerTitles").Length; ++i)
+            {
+                // First, it loads the title at i from PlayerPrefsX
+                // Second, it loads a spinner with the given title to curSpinner using the overloaded constructor
+                // Last, it adds curSpinner to the allSpinners List to keep track of the active spinners while the app is open
+                allSpinners.Add(curSpinner = new CustomSpinner(PlayerPrefsX.GetStringArray("storedSpinnerTitles")[i]));
+            }
+        }
+    }
+
+    public void CreateSpinner() // Creates a new CustomSpinner object
+    {
+        //TODO: incorporate a button to make this method functional
+        curSpinner = new CustomSpinner();
+        PlayerPrefs.Save(); // Saves the active spinners to PlayerPrefs in case of a crash
+    }
+
+    //NOTE: EditSpinner() is not planned to be finished in Sprint #2
+    public void EditSpinner() // Edit an existing CustomSpinner object
+    {
+        // TODO: incorporate a button that makes whatever spinner is selected in the app to be the curSpinner
+        PlayerPrefs.Save(); // Saves the active spinners to PlayerPrefs in case of a crash
+    }
+
+    public void DeleteSpinner() // Delete
+    {
+        // TODO: incorporate a button that makes whatever spinner is selected in the app to be the curSpinner
+        curSpinner.DeleteSpinner(); // Nullifies the contents of the spinner from PlayerPrefsX
+        allSpinners.Remove(curSpinner); // Removes curSpinner from the List of active spinners
+        PlayerPrefs.Save(); // Saves the active spinners to PlayerPrefs in case of a crash
+    }
+
+    void OnApplicationQuit() // Saves the titles of the active spinners
+    {
+        if (allSpinners.Count != 0) // Checks if there is at least one active spinner in the app
+        {
+            storedSpinnerTitles = new string[allSpinners.Count];
+
+            for (int i = 0; i < allSpinners.Count; ++i)
+            {
+                storedSpinnerTitles[i] = allSpinners[i].GetTitle(); // Grabs the titles of each of the active spinners and stores them in an array
+            }
+            PlayerPrefsX.SetStringArray("storedSpinnerTitles", storedSpinnerTitles);
+        }
+
+        allSpinners.Clear(); // Since all of the active spinners are now saved in PlayerPrefsX, we can clear the list
+    }
+}
