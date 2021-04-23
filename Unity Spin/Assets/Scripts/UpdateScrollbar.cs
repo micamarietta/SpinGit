6078c7aead39d7b882eb46e0d90c8c0a5b51590d
Cole Ternes <ternes@chapman.edu>
2021-04-23 12:02:30 -0700

    Main Menu Scrollbar
    Adds a script that disables the scrollbar on the main menu if there's less than 6 spinners

---
+++ Unity Spin/Assets/Scripts/UpdateScrollbar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateScrollbar : MonoBehaviour
+{
+    public ScrollRect scrollrect;
+
+    void Update() // Updates if the scrollbar is enabled or disabled
+    {
+        if (ProfileManager.Instance.activeSpinners.Count > 6)
+        {
+            scrollrect.enabled = true; // Enables the scrollrect
+            scrollrect.GetComponentInChildren<Scrollbar>().interactable = true; // Enables the scrollbar
+        }
+        else
+        {
+            scrollrect.enabled = false; // Disables the scrollrect
+            scrollrect.GetComponentInChildren<Scrollbar>().interactable = false; // Disables the scrollbar
+        }
+    }
+}
