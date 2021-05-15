55cd0d5872269268270fd00628d3b201375349da
Cole Ternes <ternes@chapman.edu>
2021-05-15 09:07:07 -0700

    Bug Patch 5/15/21
    This bug patch limits the number of spinners created to only 12 so that they do not exceed past the scrollbar. This bug patch also sets the character limit for the title inputfield in the EditListScreen.

---
+++ Unity Spin/Assets/Scripts/LimitSpinners.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LimitSpinners : MonoBehaviour
+{
+    public Button createSpinner; // The button to be disabled
+
+    void Update()
+    {
+        if (ProfileManager.Instance.activeSpinners.Count >= 12) // If there are 12 or more spinners, disable the create spinner button
+        {
+            createSpinner.GetComponent<Button>().interactable = false;
+        }
+        else // If there are less than 12 spinners, enable the create spinner button
+        {
+            createSpinner.GetComponent<Button>().interactable = true;
+        }
+    }
+}
